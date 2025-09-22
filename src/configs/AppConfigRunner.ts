13729bb5d18f5dd6230bbb8964eb2355fc6f64aa
Aditya Shukla <Shadowshukla76@gmail.com>
2025-09-22 09:55:27 +0530

    added mongoose for db connection

--- src/configs/AppConfigRunner.ts
+++ src/configs/AppConfigRunner.ts
@@ -1,16 +1,20 @@
 import { PORT } from "./DotEnvConfig.js";
 import { Express } from "express";
 import { logger } from "./Logger.js";
+import { DBConnect } from "../db/db.js";
 
 export const AppConnect = (app: Express) => {
   try {
-     app.listen(PORT||8080, () => {
+    app.listen(PORT || 8080, () => {
       console.log(
-        `Server started at PORT: ${PORT||8080} and you can see here http://localhost:${PORT||8080}`
+        `Server started at PORT: ${
+          PORT || 8080
+        } and you can see here http://localhost:${PORT || 8080}`
       );
     });
+    DBConnect();
   } catch (error) {
-    logger.error(`Error in Starting the server at PORT: ${PORT||8080}`);
-    console.error(`Error in Starting the server at PORT: ${PORT||8080}`);
+    logger.error(`Error in Starting the server at PORT: ${PORT || 8080}`);
+    console.error(`Error in Starting the server at PORT: ${PORT || 8080}`);
   }
 };
